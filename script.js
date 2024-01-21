d3fefb9a55d4211296ae4935d1249b840e34eddd
Ilkin Hamzayev <ilkinhamzayev925@gmail.com>
2024-01-21 23:18:37 +0100

    Done

---
+++ script.js
@@ -0,0 +1,64 @@
+//Getting the DOM elements
+
+//Function that updates the current step and updates the DOM
+
+//Loop through circles and add/remove "active" class based on their index and current step
+
+ //Checking and disabling prev and next buttons
+
+ // Update progress bar width based on current step
+
+//Add Click event listeners to all buttons
+
+
+const circles = document.querySelectorAll('.circle');
+const buttons = document.querySelectorAll('button');
+const progressBar = document.querySelector('.indicator');
+
+// console.log(circles,buttons, progressBar);
+
+let currentStep = 1;
+
+
+const updateSteps = (e) =>{
+
+    currentStep = e.target.id === "next" ? ++currentStep : --currentStep;
+    // console.log(currentStep);
+
+    circles.forEach((circle, index) => {
+        circle.classList[`${index < currentStep ? 'add' : 'remove'}`]('active');
+        
+        if(currentStep === circles.length){
+            buttons[1].disabled = true;
+        } else if (currentStep === 1){
+            buttons[0].disabled = true;
+        } else{
+            buttons.forEach(button =>{
+                button.disabled = false;
+            })
+        }
+    })
+
+
+    progressBar.style.width = `${(currentStep -1) /(circles.length - 1) * 100}%`
+
+
+}
+
+
+
+
+
+buttons.forEach( button =>{
+button.addEventListener('click', updateSteps);
+})
+
+
+
+
+
+
+
+
+
+
